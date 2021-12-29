0064e15907d0f3f3b256f394e024459c9100ac3f
MyteDr <dogukan_candan@hotmail.com>
2021-12-29 19:53:40 +0300

    First Commit

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function Onclick(){
     }
     else{
         if(gb.value == 'GB'){
-            if(mbps.value =='MBps')
+            if(mbps.value =='MB/sec')
             {
                 calc(file.value,speed.value,true,true);
             }
@@ -24,7 +24,7 @@ function Onclick(){
         }
         else
         {
-            if(mbps.value =='MBps')
+            if(mbps.value =='MB/sec')
             {
                 calc(file.value,speed.value,false,true);
             }
@@ -42,11 +42,11 @@ function calc(file,speed,gb,mbps){
         alert('Please Do Not Enter Zero!');
     }
     else{
-        let size= gb ? parseInt(file)*1000 : parseInt(file);
-        let speed2 = mbps ? parseInt(speed) : parseInt(speed)/8;
+        let size= gb ? parseFloat(file)*1000 : parseFloat(file);
+        let speed2 = mbps ? parseFloat(speed) : parseFloat(speed)/8;
         let Hour =parseInt(size/speed2/3600);
         let Minute = parseInt(size/speed2/60 % 60);
         let Seconds = parseInt(size/speed2 %3600) %60;
-        result.textContent=`${Hour} Hour ${Minute} Minute ${Seconds} Seconds`;
+        result.textContent=`${Hour == 0 ? '' : (Hour+(Hour == 1 ? ' Hour' :' Hours'))} ${Minute == 0 ? '' : (Minute+(Minute ==1 ? ' Minute' : ' Minutes'))} ${Seconds == 0 ? '':(Seconds+(Seconds == 1 ? ' Second' : ' Seconds'))}`;
     }
 }
