84e7b07eb6c9cd77b48d0e8d913d7c1cad98c6a4
MyteDr <92827502+MyteDr@users.noreply.github.com>
2022-01-17 00:30:44 +0300

    First Commit

--- main.js
+++ main.js
@@ -1,41 +1,13 @@
-const speed = document.getElementById('speed');
-const file = document.getElementById('file');
-const gp =document.getElementById('gb');
-const mbps =document.getElementById('mbps');
-const result =document.getElementById('result');
-const button = document.getElementById('press');
-
-button.addEventListener('click',Onclick);
-function Onclick(){
-    if(file.value=='' || speed.value=='')
-    {
-        alert('Please Do Not Leave The Field Blank!');
-    }
-    else{
-        if(gb.value == 'GB'){
-            if(mbps.value =='MB/sec')
-            {
-                calc(file.value,speed.value,true,true);
-            }
-            else
-            {
-                calc(file.value,speed.value,true,false);
-            }
+$(document).ready(function(){
+    $('#press').click(function(){
+        if(($('#file').val() =='') || ($('#speed').val() =='')){
+            alert('Please Do Not Leave The Field Blank!');
         }
-        else
-        {
-            if(mbps.value =='MB/sec')
-            {
-                calc(file.value,speed.value,false,true);
-            }
-            else
-            {
-                calc(file.value,speed.value,false,false);
-            }
+        else{
+            $('#gb').val()=='GB' ? ($('#mbps').val()=='MB/sec' ? calc(file.value,speed.value,true,true) : calc(file.value,speed.value,true,false)):($('#mbps').val()=='MB/sec' ? calc(file.value,speed.value,false,true):calc(file.value,speed.value,false,false));
         }
-    }
-
-}
+    });
+});
 function calc(file,speed,gb,mbps){
     if(file =="0" || speed=="0")
     {
@@ -47,6 +19,6 @@ function calc(file,speed,gb,mbps){
         let Hour =parseInt(size/speed2/3600);
         let Minute = parseInt(size/speed2/60 % 60);
         let Seconds = parseInt(size/speed2 %3600) %60;
-        result.textContent=`${Hour == 0 ? '' : (Hour+(Hour == 1 ? ' Hour' :' Hours'))} ${Minute == 0 ? '' : (Minute+(Minute ==1 ? ' Minute' : ' Minutes'))} ${Seconds == 0 ? '':(Seconds+(Seconds == 1 ? ' Second' : ' Seconds'))}`;
+        $('#result').html(`${Hour == 0 ? '' : (Hour+(Hour == 1 ? ' Hour' :' Hours'))} ${Minute == 0 ? '' : (Minute+(Minute ==1 ? ' Minute' : ' Minutes'))} ${Seconds == 0 ? '':(Seconds+(Seconds == 1 ? ' Second' : ' Seconds'))}`);
     }
 }
